37647c14773dcb4db88a3f5ec508210c208f4408
tommyk154 <tkargul1@binghamton.edu>
2016-02-14 23:39:19 +0000

    Implemented guess(letter) function

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -10,8 +10,12 @@ class HangpersonGame
   
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
   end
 
+  attr_accessor :word, :guesses, :wrong_guesses
+  
   def self.get_random_word
     require 'uri'
     require 'net/http'
@@ -19,4 +23,14 @@ class HangpersonGame
     Net::HTTP.post_form(uri ,{}).body
   end
 
+  def guess(letter)
+    raise ArgumentError if !(letter =~ /[a-z]{1}/i)
+    return false if (@guesses =~ /#{letter}/i)  or (@wrong_guesses =~ /#{letter}/i)
+    if (@word =~ /#{letter}/i)
+      @guesses += letter.downcase
+    else
+      @wrong_guesses += letter.downcase
+    end
+  end
+  
 end
